862ab5ddc3bc55d7ce5a9ccd6afb2b14d2e4d1f7
John Lombardo <john@programbo.com>
2017-07-28 08:50:20 +0800

    First commit

---
+++ main/index.ts
@@ -0,0 +1,31 @@
+// Native
+const { join } = require('path')
+const { format } = require('url')
+
+// Packages
+const { BrowserWindow, app } = require('electron')
+const isDev = require('electron-is-dev')
+const prepareNext = require('electron-next')
+
+// Prepare the renderer once the app is ready
+app.on('ready', async () => {
+  await prepareNext('./build/renderer')
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  const url = isDev
+    ? 'http://localhost:8000/start'
+    : format({
+        pathname: join(__dirname, '../renderer/start/index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+
+  mainWindow.loadURL(url)
+})
+
+// Quit the app once all windows are closed
+app.on('window-all-closed', app.quit)
---
+++ renderer/pages/start.tsx
@@ -0,0 +1,2 @@
+import * as React from 'react'
+export default () => <span>This is Next.js speaking, buddy!</span>
