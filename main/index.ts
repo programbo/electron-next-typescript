03c28a8fccf51d7585e2e568843a44e4b29909a5
John Lombardo <john@programbo.com>
2017-07-28 11:12:21 +0800

    Refine dist output

--- main/index.ts
+++ main/index.ts
@@ -9,7 +9,7 @@ const prepareNext = require('electron-next')
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
-  await prepareNext('./build/renderer')
+  await prepareNext('./renderer')
 
   const mainWindow = new BrowserWindow({
     width: 800,
